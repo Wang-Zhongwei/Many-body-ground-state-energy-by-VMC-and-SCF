30d774f2d47b9ce4ab773db0d6606f9590dd6f16
Wang-Zhongwei <jonathan_wang1999@icloud.com>
2022-12-11 20:12:54 -0500

    chore: interface between simulated_annealing and markov chain

--- src/main/annealing.py
+++ src/main/annealing.py
@@ -1,20 +1,26 @@
+import random
+
 import numpy as np
 
 
-import random
-
-def simulated_annealing(initial_state, cost_function, neighborhood_function, temperature_function, max_iterations):
+def simulated_annealing(initial_state, cost_function, neighborhood_function, temperature_function, max_iterations, **kwargs):
     state = initial_state
-    cost = cost_function(state)
+    cost = cost_function(state, **kwargs)[0]
+    accept = 0
     for i in range(max_iterations):
         temperature = temperature_function(i)
         next_state = neighborhood_function(state)
-        next_cost = cost_function(next_state)
+        next_cost = cost_function(next_state, **kwargs)[0]
         delta_cost = next_cost - cost
         if delta_cost > 0 and random.random() > np.exp(-delta_cost / temperature): 
             continue
+
         state = next_state
         cost = next_cost
+        accept += 1
+        print('Accepted count %d: (alpha, beta) = %s, cost = %f' % (accept, state, cost))
+    
+    print(f'Minimized cost {cost} with acceptance rate {accept/max_iterations}')
     return state
 
 
--- src/main/vmc.py
+++ src/main/vmc.py
@@ -4,10 +4,9 @@
 import math
 import sys
 from random import random
+from functools import partial
 
-# import annealing
 import numpy as np
-
 from annealing import *
 
 # Read name of output file from command line
@@ -15,31 +14,31 @@ if len(sys.argv) != 2:
     print('Usage: python he.py <out_file>')
     sys.exit(1)
 outfileName = sys.argv[1]
-r_abs = lambda r: np.sqrt(np.sum(r**2, axis=-1))
+
 # Trial wave function
-def wave_function(r: np.ndarray):
+def wave_function(r: np.ndarray, alpha, beta):
+    argument = 0.0
 
-    # single body part
-    wf = np.exp(-alpha * np.sum(r_abs(r)))
+    for i in range(NUM_OF_PARTICLES):
+        r_single_particle = 0.0
+        for j in range(DIMENSION):
+            r_single_particle += r[i, j]**2
+        argument += math.sqrt(r_single_particle)
 
-    # two body part
-    for i in range(r.shape[0]):
-        for j in range(i+1, r.shape[0]):
-            r_ij = r_abs(r[i] - r[j])
-            wf *= np.exp(r_ij / (1 + beta * r_ij))
-    return wf
+    return math.exp(-argument*alpha)
 
 # Local energy (numerical derivative)
 # the argument wf is the wave function value at r (so we don't need to calculate it again)
 
 
-def local_energy(r, wf):
+def local_energy(r, wave_function):
     # Kinetic energy
     r_plus = r.copy()
     r_minus = r.copy()
 
     e_kinetic = 0.0
 
+    wf = wave_function(r)
     for i in range(NUM_OF_PARTICLES):
         for j in range(DIMENSION):
             r_plus[i, j] = r[i, j] + h
@@ -72,35 +71,41 @@ def local_energy(r, wf):
 
     return e_potential + e_kinetic
 
-def evaluate_energy(initial_state: np.ndarray, wave_function, neighborhood_function, num_cycles: int, thermalization: int):
-    # markov chain monte carlo
+def evaluate_energy(wf_params, num_cycles: int, thermalization: int):
+    # Bind wave function to wf_params
+    alpha, beta = wf_params
+    concrete_wave_func = partial(wave_function, alpha=alpha, beta=beta)
 
-    # initialize state and wave function
-    state = initial_state
-    wf = wave_function(state)
-    # Initialize energy and energy squared
+    # Initialization
+    r = ELEC_STEP_LENGTH * np.random.uniform(-1, 1, (NUM_OF_PARTICLES, DIMENSION))
+    wf = concrete_wave_func(r)
     energy = energy2 = 0.0
-    # Initialize acceptance counter
     accept = 0
-    for cycle in range(num_cycles+thermalization):
+
+    for cycle in range(num_cycles + thermalization):
         # Trial position
-        next_state = neighborhood_function(state)
-        next_wf = wave_function(next_state)
+        r_trial = r + ELEC_STEP_LENGTH * np.random.uniform(-1, 1, (NUM_OF_PARTICLES, DIMENSION))
+        wf_trial = concrete_wave_func(r_trial)
+
         # Metropolis test to see whether we accept the move
-        if random() < next_wf**2 / wf**2:
-            state = next_state
-            wf = next_wf
+        if np.random.rand() < wf_trial**2 / wf**2:
+            r = r_trial
+            wf = wf_trial
             accept += 1
-        # If we are done with thermalization, we add to the statistics
+
+        # Add to stats when done with thermalization
         if cycle >= thermalization:
-            delta_e = local_energy(state, wf)
+            delta_e = local_energy(r, concrete_wave_func)
             energy += delta_e
             energy2 += delta_e**2
-    # Return the average energy and its error and acceptance rate
+
+    # Calc average energy, error and acceptance rate
     energy /= num_cycles
     energy2 /= num_cycles
     error = np.sqrt(energy2 - energy**2)
-    return energy, error, accept/num_cycles
+    acceptance = accept / (num_cycles + thermalization)
+
+    return energy, error ,acceptance
 
 
 # Here starts the main program
@@ -109,41 +114,16 @@ CHARGE = 2
 DIMENSION = 3
 MAX_VARIATIONS = 10
 THERMALIZATION = 10
-NUM_OF_CYCLES = 10000 # one less zero during test
-R_STEP_LENGTH = 1.0
+NUM_OF_CYCLES = 100000
+ELEC_STEP_LENGTH = 1.0
 PARAM_STEP_LENGTH = 0.1
 
 alpha = 0.5 * CHARGE  # variational parameter
-beta = 0.0
 
 # Step length for numerical differentiation and its inverse squared
 h = .001
 h2 = 1/(h**2)
 
-r_old = np.zeros((NUM_OF_PARTICLES, DIMENSION), np.double)
-r_new = np.zeros((NUM_OF_PARTICLES, DIMENSION), np.double)
-
-# Loop over alpha values
-outfile = open(outfileName, 'w')
-for variate in range(MAX_VARIATIONS):
-
-    alpha += .1
-    energy = energy2 = 0.0
-    accept = 0.0
-    delta_e = 0.0
-
-    # Initial position
-    energy, error, accept = evaluate_energy(initial_state, wave_function, lambda r: r + R_STEP_LENGTH * np.random.uniform(-1, 1, (NUM_OF_PARTICLES, DIMENSION)), NUM_OF_CYCLES, THERMALIZATION)
-
-    # For a given alpha, write results to file
-    outfile.write('%f %f %f %f\n' % (alpha, energy,
-                    error, accept*1.0/(NUM_OF_CYCLES+THERMALIZATION)))
-outfile.close()
-
-initial_state = R_STEP_LENGTH * np.random.uniform(-1, 1, (NUM_OF_PARTICLES, DIMENSION))
-neighborhood_func = lambda r: r + PARAM_STEP_LENGTH * np.random.uniform(-1, 1, (NUM_OF_PARTICLES, DIMENSION))
-temperature_func = lambda i: 1.0/(i+1)
-simulated_annealing(initial_state, evaluate_energy, neighborhood_func, temperature_func, 20)
-
-print('\nDone. Results are in the file "%s", formatted as:\n\
-alpha, <energy>, error, acceptance ratio' % outfileName)
+initial_state = np.array([1.5, 0]) # initial state of (alpha, beta) 
+neighbor_func = lambda v: v + np.random.uniform(-1, 1, 2) * PARAM_STEP_LENGTH
+simulated_annealing(initial_state, evaluate_energy, neighbor_func, temperature_function=lambda i: 0.05/(i+1), max_iterations=20, num_cycles=NUM_OF_CYCLES, thermalization=THERMALIZATION)
